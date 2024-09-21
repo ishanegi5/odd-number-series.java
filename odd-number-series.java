6a417b92bfc395baacc39ac26cef4666a6dc6369
Isha Negi <itsishanegi@gmail.com>
2024-09-22 01:42:35 +0530

    Add files via upload

---
+++ odd-number-series.java
@@ -0,0 +1,16 @@
+public class Main{
+    
+public static void main(String[]args){
+    
+int a=0;
+    
+do{
+        
+int n=(2*a)+1;
+       
+System.out.println(n);
+        
+a++;
+   
+}while(a<=4);
+    
